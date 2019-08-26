c716512b0999d3595012836dfc71c37faa3f8c1b
Maarten Andersson <marten.andersson@zhipster.se>
2019-08-26 15:51:10 +0200

    updated git update submodules

--- Functions/git/GitShortcuts.ps1
+++ Functions/git/GitShortcuts.ps1
@@ -1,61 +1,61 @@
 
 #Git shortcuts
 function fetch {
-    git.exe fetch --all --verbose
+    git fetch --all --verbose
 }
 function master {
-    git.exe fetch origin 
-    git.exe checkout master
+    git fetch origin 
+    git checkout master
 }
 function dev {
-    git.exe fetch origin
-    git.exe checkout dev
+    git fetch origin
+    git checkout dev
 }
 function new-release($name) {
     $name = "release/$($name)"
-    git.exe pull
-    git.exe checkout -b "$($name)"
-    git.exe push -u origin "$($name)"
+    git pull
+    git checkout -b "$($name)"
+    git push -u origin "$($name)"
 }
 
 function new-feature($name) {
     $name = "feature/$($name)"
-    git.exe checkout -b "$($name)"
-    git.exe push -u origin "$($name)"
+    git checkout -b "$($name)"
+    git push -u origin "$($name)"
 }
 function new-hotfix($name) {
     $name = "hotfix/$($name)"
-    git.exe checkout -b "$($name)"
-    git.exe push -u origin "$($name)"
+    git checkout -b "$($name)"
+    git push -u origin "$($name)"
 }
 
 function glog {
     Write-Host "Print git log and with oneline custom format"
-    git.exe log --pretty=format:'%h %ad %s | %an' --date=short
+    git log --pretty=format:'%h %ad %s | %an' --date=short
 }
 
 
 
 function gs {
-    git.exe status
+    git status
 }
 function clone($url) {
-    git.exe clone --recursive -j 8 $url
+    git clone --recursive -j 8 $url
 }
 function update() {
-    git.exe pull
-    git.exe submodule update --recursive
+    git pull
+    git submodule update --recursive --remote --init
 }
 function prune-check() {
-    git.exe remote prune origin --dry-run
+    git remote prune origin --dry-run
 }
 function prune() {
-    git.exe remote prune origin
+    git remote prune origin
 }
 
 function gco($message) {
-    git.exe add . | Out-Host
-    git.exe commit -am $message | Out-Host
+    git add . | Out-Host
+    git commit -am $message | Out-Host
 }
 
 function git-cherry($url) {
@@ -73,10 +73,10 @@ function git-cherry($url) {
     $urlData = $url.Split("/")
     $repo = "$($urlData[3])/$($urlData[4])"
     $hash = $urlData[6]
-    git.exe remote add $repo "git@github.com:$($repo).git"
-    git.exe fetch $repo
-    git.exe cherry-pick $hash -x
-    git.exe status
+    git remote add $repo "git@github.com:$($repo).git"
+    git fetch $repo
+    git cherry-pick $hash -x
+    git status
     # $KeyOption = 'Y','N'
     # while ($KeyOption -notcontains $KeyPress.Character) {
     #  $KeyPress = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
