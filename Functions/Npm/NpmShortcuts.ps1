8284cda1a3f573793fbd2cc881b1098031120a6f
Maarten Andersson <marten.andersson@zhipster.se>
2019-08-28 15:15:28 +0200

    updated npm publishing scripts

--- Functions/Npm/NpmShortcuts.ps1
+++ Functions/Npm/NpmShortcuts.ps1
@@ -1,12 +1,15 @@
-function npm-patch() {
+function npm-publish-patch() {
+    npm run pre-publish
     npm version patch
     npm publish
 }
-function npm-minor() {
+function npm-publish-minor() {
+    npm run pre-publish
     npm version minor
     npm publish
 }
-function npm-major() {
+function npm-publish-major() {
+    npm run pre-publish
     npm version major
     npm publish
 }
