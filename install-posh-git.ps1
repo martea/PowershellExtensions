eb7b5c3fa50574137596bdcb33655e20e7c8a820
unknown <maarten.anderssson@gmail.com>
2019-10-28 08:11:02 +0100

    updated powershell installation scirpt with oh my posh, added script for windows update also

---
+++ install-posh-git.ps1
@@ -0,0 +1,35 @@
+If (Get-Module -ListAvailable -Name "posh-git") {
+    Write-Host "posh-git already installed" -ForegroundColor Cyan 
+}
+else {
+    Write-Host "installing posh-git" -ForegroundColor Cyan 
+    PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+    PowerShellGet\Install-Module oh-my-posh -Scope CurrentUser -Force
+    PowerShellGet\Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+}
+
+if (!(Test-Path $PROFILE)) {
+    # file with path $path doesn't exist
+    New-Item -ItemType file $Profile
+}
+function hasContent($text) {
+    $file = Get-Content $PROFILE
+    $containsWord = $file | ForEach-Object { $_ -match $text }
+    if ($containsWord -contains $true) {
+        return $true;
+    }
+    else {
+        return $false;
+    }
+}
+$installHeader = "Posh-git module loader"
+
+if (!(hasContent($installHeader))) {
+    Write-Host "adding $installHeader" -ForegroundColor Cyan 
+    Add-Content $PROFILE "# $installHeader"
+    Add-Content $PROFILE "Import-Module posh-git"
+    Add-Content $PROFILE "Import-Module oh-my-posh"
+    Add-Content $PROFILE "Set-Theme Paradox"
+    Write-host "Dont forget to add font https://github.com/adam7/delugia-code/releases";
+    explorer.exe .\fonts       
+}
--- install.ps1
+++ install.ps1
@@ -1,39 +1,6 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-If (Get-Module -ListAvailable -Name "posh-git") {
-    Write-Host "posh-git already installed" -ForegroundColor Cyan 
-}
-else {
-    Write-Host "installing posh-git" -ForegroundColor Cyan 
-    PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
-    PowerShellGet\Install-Module oh-my-posh -Scope CurrentUser -Force
-    PowerShellGet\Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
-}
 
-if (!(Test-Path $PROFILE)) {
-    # file with path $path doesn't exist
-    New-Item -ItemType file $Profile
-}
-function hasContent($text) {
-    $file = Get-Content $PROFILE
-    $containsWord = $file | ForEach-Object { $_ -match $text }
-    if ($containsWord -contains $true) {
-        return $true;
-    }
-    else {
-        return $false;
-    }
-}
-$installHeader = "Posh-git module loader"
-
-if (!(hasContent($installHeader))) {
-    Write-Host "adding $installHeader" -ForegroundColor Cyan 
-    Add-Content $PROFILE "# $installHeader"
-    Add-Content $PROFILE "Import-Module posh-git"
-    Add-Content $PROFILE "Import-Module oh-my-posh"
-    Add-Content $PROFILE "Set-Theme Paradox"
-    Write-host "Dont forget to add font https://github.com/adam7/delugia-code/releases";
-}
-
+. ./install-posh-git.ps1
 
 
 $installHeader = "Autoload Script"
@@ -46,3 +13,5 @@ if (!(hasContent($installHeader))) {
     Add-Content $PROFILE "# $installHeader"
     Add-Content $PROFILE $psScript
 }
+
+. ./update-windows.ps1;
