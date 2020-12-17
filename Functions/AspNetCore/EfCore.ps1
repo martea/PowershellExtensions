a10874aa1fb2f2982090c97bed08e523151b76a4
Mårten Andersson <marten.andersson@egain.io>
2020-12-17 11:45:42 +0100

    update set env

--- Functions/AspNetCore/EfCore.ps1
+++ Functions/AspNetCore/EfCore.ps1
@@ -5,6 +5,22 @@ function Add-EFC-Migration {
   )
   dotnet ef migrations add $name --verbose
 }
+function Add-EFC-Migration {
+  Param(
+    [Parameter(Mandatory = $true)]
+    [string]$name,
+    [Parameter(Mandatory = $true)]
+    [string]$dbcontext,
+    [Parameter(Mandatory = $true)]
+    [string]$project,
+    [Parameter(Mandatory = $true)]
+    [string]$startupProject
+  )
+  # -c | --context <DBCONTEXT>               The DbContext to use.
+  # -p | --project <PROJECT>                 The project to use.
+  # -s | --startup-project <PROJECT>  
+  dotnet ef migrations add $name --verbose -c $dbcontext -p $project -s $startupProject
+}
 function Update-EFC-Migration {
   dotnet ef database update --verbose
 }
